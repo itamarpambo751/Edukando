66511e49ab5836533ba46dbe3c832c4bc4f2f032
itamarpambo <itamarpambo751@gmail.com>
2023-11-27 22:08:40 +0100

    View Team Member Image

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,66 +1,73 @@
 import styled from "styled-components"
-import team_member_1 from "../../assets/images/blog/blog_6.png"
-import team_member_2 from "../../assets/images/blog/blog_7.png"
-import team_member_3 from "../../assets/images/blog/blog_8.png"
-import team_member_4 from "../../assets/images/blog/blog_9.png"
-import team_member_5 from "../../assets/images/blog/blog_25.png"
-import team_member_6 from "../../assets/images/blog/blog_20.png"
-import team_member_7 from "../../assets/images/blog/blog_28.jpg"
-import team_member_8 from "../../assets/images/blog/blog_30.jpg"
+import { BsChatLeftFill, BsEye, BsEyeFill, BsX } from "react-icons/bs"
+import { useState } from "react"
+import image from "../../assets/images/blog/blog_6.png"
+import image2 from "../../assets/images/blog/blog_7.png"
+import image3 from "../../assets/images/blog/blog_8.png"
+import image4 from "../../assets/images/blog/blog_9.png"
+import image5 from "../../assets/images/blog/blog_25.png"
+import image6 from "../../assets/images/blog/blog_20.png"
+import image7 from "../../assets/images/blog/blog_28.jpg"
+import image8 from "../../assets/images/blog/blog_30.jpg"
+
+
+const team = [
+    {
+        image: image,
+    },
+    {
+        image: image2,
+    },
+    {
+        image: image3,
+    },
+    {
+        image: image4,
+    },
+    {
+        image: image5,
+    },
+    {
+        image: image6,
+    },
+    {
+        image: image7,
+    },
+    {
+        image: image8,
+    },
+]
 
 export default function Team() {
+    const [viewing, setViewing] = useState()
+    function viewThis(member) {
+        setViewing(member.image)
+    }
     return(
         <StyledTeamContainer>
             <div id="team_gallery">
-                <figure>
-                    <img src={team_member_4} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_3} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_2} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_1} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_5} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_6} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_7} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
-                <figure>
-                    <img src={team_member_8} alt="team_member" />
-                    <figcaption>
-
-                    </figcaption>
-                </figure>
+                {team.map(member => (
+                    <figure>
+                        <img src={member.image} alt="team_member" />
+                        <figcaption>
+                            <i onClick={() => viewThis(member)}>
+                                <BsEyeFill/>
+                            </i>
+                        </figcaption>
+                    </figure>
+                ))}
             </div>
+            {viewing && (
+                <ViewTeamMember>
+                   
+                    <div className="img_container">
+                        <button id="img_container_close" onClick={() => setViewing(null)}>
+                            <i><BsX/></i>
+                        </button>
+                        <img src={viewing} alt="team_member" />
+                    </div>
+                </ViewTeamMember>
+            )}
         </StyledTeamContainer>
     )
 }
@@ -68,7 +75,6 @@ export default function Team() {
 const StyledTeamContainer = styled.div`
     width: 100%;
     margin-top: 100px;
-
     #team_gallery {
         width: 90%;
         padding: 2rem;
@@ -92,7 +98,65 @@ const StyledTeamContainer = styled.div`
                 width: 100%;
                 height: 100%;
                 z-index: 999;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: rgba(0, 0, 0, .5);
+                opacity: 0;
+                transition: all.3s ease;
+                &:hover {
+                    opacity: 1;
+                }
+                i {
+                    color: white;
+                    cursor: pointer;
+                }
             }
+        }
+    }
+`
+const ViewTeamMember = styled.div`
+    text-align: center;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    padding: 0 8px;
+    box-sizing: border-box;
+    background-color: rgba(0, 0, 0, .8);
+    z-index: 9999;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    .img_container {
+        position: relative;
+        width: 500px;
+        height: 400px;
+        object-fit: cover;
+        #img_container_close {
+            position: absolute;
+            top: -40px;
+            right: 0;
+            width: 40px;
+            height: 40px;
+            color: white;
+            background: rgba(30, 30, 50, .4);
+            cursor: pointer;
+            i svg {
+                margin-top: 3px;
+                font-size: 20px;
+            }
+        }
+        img {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            left: 0;
+            top: 0;
         }
     }
 `
