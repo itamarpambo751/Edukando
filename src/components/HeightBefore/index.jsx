4e04c8a3a61a00afd1834ca8d30f0139e446cb3f
itamarpambo <itamarpambo751@gmail.com>
2023-12-30 19:16:30 +0100

    Decoração de texto componentizada

---
+++ src/components/HeightBefore/index.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export default function HeightBefore({ children }) {
+    return <Text>{children}</Text>
+}
+const Text = styled.span`
+    display: block;
+    background: var(--whiteColor);
+    display: flex;
+    border-left: 5px solid var(--primaryColor);
+    max-width: 400px;
+    align-items: center;
+    justify-content: center;
+    padding: 25px 10px;
+    position: absolute;
+    left: 0;
+    box-shadow: 0 0 50px 10px rgba(95, 45, 237, 0.15);
+    animation: move5 3s infinite linear;
+`
